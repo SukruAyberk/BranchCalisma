70fe3266126c99084bb864ed4e790d677c16e888
Sevket Bugra Yilmaz <sbugra4@gmail.com>
2024-02-11 18:13:34 +0300

    Sevket Commit

---
+++ src/test/java/pages/HelloWorld.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.testng.annotations.Test;
+import utilities.Driver;
+import utilities.TestBase;
+
+public class HelloWorld extends TestBase {
+    @Test
+    public void HelloWorld(){
+
+        System.out.println("Hello World");
+    }
+
+
+}
+
